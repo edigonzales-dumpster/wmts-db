fa70859a4b5309b43d9981f50d66184fbda37300
Stefan Ziegler <edi.gonzales@gmail.com>
2019-11-19 19:07:46 +0100

    add view

--- sql/__work.sql
+++ sql/__work.sql
@@ -12,3 +12,19 @@ FROM
     RIGHT JOIN agi_dm01avso24.einzelobjekte_flaechenelement AS flaechenelement
     ON flaechenelement.flaechenelement_von = einzelobjekt.t_id
 ;
+
+
+CREATE OR REPLACE VIEW 
+    agi_dm01avso24.vw_einzelobjekte_linienlelement
+AS 
+SELECT
+    linienelement.t_id, 
+    einzelobjekt.qualitaet,
+    einzelobjekt.art,
+    einzelobjekt.entstehung,
+    linienelement.geometrie
+FROM
+    agi_dm01avso24.einzelobjekte_einzelobjekt AS einzelobjekt
+    RIGHT JOIN agi_dm01avso24.einzelobjekte_linienelement AS linienelement
+    ON linienelement.linienelement_von = einzelobjekt.t_id
+;
