7c25408fdb4a6f5aad9f6eb4cf65c53e43747ecb
Stefan Ziegler <edi.gonzales@gmail.com>
2019-11-20 20:10:04 +0100

    more views

--- sql/__work.sql
+++ sql/__work.sql
@@ -166,3 +166,83 @@ FROM
     RIGHT JOIN agi_dm01avso24.gebaeudeadressen_hausnummerpos AS pos
     ON eingang.t_id = pos.hausnummerpos_von
 ;
+
+CREATE OR REPLACE VIEW 
+    agi_dm01avso24.vw_nomenklatur_flurnamepos
+AS 
+SELECT
+    pos.t_id,
+    pos.hali,
+    pos.vali,
+    pos.ori,
+    pos.pos,
+    flurname.aname
+FROM
+    agi_dm01avso24.nomenklatur_flurnamepos AS pos
+    LEFT JOIN agi_dm01avso24.nomenklatur_flurname AS flurname
+    ON pos.flurnamepos_von = flurname.t_id
+;
+
+CREATE OR REPLACE VIEW 
+    agi_dm01avso24.vw_nomenklatur_gelaendenamepos
+AS 
+SELECT
+    pos.t_id,
+    pos.hali,
+    pos.vali,
+    pos.ori,
+    pos.pos,
+    gelaendename.aname
+FROM
+    agi_dm01avso24.nomenklatur_gelaendenamepos AS pos
+    LEFT JOIN agi_dm01avso24.nomenklatur_gelaendename AS gelaendename
+    ON pos.gelaendenamepos_von = gelaendename.t_id
+;
+
+CREATE OR REPLACE VIEW 
+    agi_dm01avso24.vw_nomenklatur_ortsnamepos
+AS 
+SELECT
+    pos.t_id,
+    pos.hali,
+    pos.vali,
+    pos.ori,
+    pos.pos,
+    ortsname.aname
+FROM
+    agi_dm01avso24.nomenklatur_ortsnamepos AS pos
+    LEFT JOIN agi_dm01avso24.nomenklatur_ortsname AS ortsname
+    ON pos.ortsnamepos_von = ortsname.t_id
+;
+
+CREATE OR REPLACE VIEW 
+    agi_dm01avso24.vw_bodenbedeckung_objektnamepos
+AS 
+SELECT
+    pos.t_id,
+    pos.hali,
+    pos.vali,
+    pos.ori,
+    pos.pos,
+    objektname.aname
+FROM
+    agi_dm01avso24.bodenbedeckung_objektnamepos AS pos
+    LEFT JOIN agi_dm01avso24.bodenbedeckung_objektname AS objektname
+    ON pos.objektnamepos_von = objektname.t_id
+;
+
+CREATE OR REPLACE VIEW 
+    agi_dm01avso24.vw_rohrleitungen_leitungsobjektpos
+AS 
+SELECT
+    pos.t_id,
+    pos.hali,
+    pos.vali,
+    pos.ori,
+    pos.pos,
+    leitungsobjekt.betreiber
+FROM
+    agi_dm01avso24.rohrleitungen_leitungsobjektpos AS pos
+    LEFT JOIN agi_dm01avso24.rohrleitungen_leitungsobjekt AS leitungsobjekt
+    ON pos.leitungsobjektpos_von = leitungsobjekt.t_id
+;
